1468951cfa1bbd54b806af97f32692deeeec3fae
formateur1 <enseignant.formateur@protonmail.com>
2022-11-04 10:18:58 +0100

    Début du projet

---
+++ src/main/java/com/inti/SpringTd3Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTd3Application.class, args);
+	}
+
+}
