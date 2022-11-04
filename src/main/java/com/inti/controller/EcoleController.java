2198526c5011f99314025b6529ccda762c923fce
formateur1 <enseignant.formateur@protonmail.com>
2022-11-04 10:54:32 +0100

    Ajout des controllers sans fonctionnalités

---
+++ src/main/java/com/inti/controller/EcoleController.java
@@ -0,0 +1,10 @@
+package com.inti.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "ecole")
+public class EcoleController {
+
+}
